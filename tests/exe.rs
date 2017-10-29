510090f51f60e661802e0ea4032bc8c3f2907ac4
Joe Ranweiler <joe@lemma.co>
2017-10-29 15:01:09 -0700

    Replace shell script exe tests
    We extend the integration test support module to make it easy to make
    assertions about command status and the contents of stderr and stdout.

---
+++ tests/exe.rs
@@ -0,0 +1,54 @@
+mod support;
+
+use support::{assert, exe, fixture};
+
+
+#[test]
+fn test_help() {
+    let out = exe::run(&["--help"]);
+
+    assert::exit_ok(&out);
+    assert::stdout_includes(&out, "USAGE");
+}
+
+#[test]
+fn test_positional() {
+    let path = &fixture::path("all_numeric_lines");
+
+    assert::exit_fail(&exe::run(&[]));
+    assert::exit_ok(&exe::run(&[path]));
+    assert::exit_ok(&exe::run(&[path, path]));
+    assert::exit_ok(&exe::run(&[path, path, path]));
+}
+
+#[test]
+fn test_stdin() {
+    let file = fixture::file("all_numeric_lines");
+    let out = exe::run_with_stdin(file, &["-s"]);
+
+    assert::exit_ok(&out);
+}
+
+#[test]
+fn test_lax() {
+    {
+        let path = &fixture::path("all_numeric_lines");
+        assert::exit_ok(&exe::run(&[path]));
+        assert::exit_ok(&exe::run(&[path, "--lax"]));
+    }
+    {
+        let path = &fixture::path("bad_lines");
+        assert::exit_fail(&exe::run(&[path]));
+        assert::exit_ok(&exe::run(&[path, "--lax"]));
+    }
+    {
+        let path = &fixture::path("empty_lines");
+        assert::exit_ok(&exe::run(&[path]));
+        assert::exit_ok(&exe::run(&[path, "--lax"]));
+    }
+    {
+        let path = &fixture::path("trailing_empty_lines");
+        assert::exit_ok(&exe::run(&[path]));
+        assert::exit_ok(&exe::run(&[path, "--lax"]));
+    }
+}
