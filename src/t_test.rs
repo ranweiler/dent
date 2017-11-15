cef939a83096fe667f46847deb0c13d41c1f4ae5
Joe Ranweiler <joe@lemma.co>
2017-11-15 13:09:12 -0800

    Document `t_test` module

--- src/t_test.rs
+++ src/t_test.rs
@@ -2,6 +2,7 @@ use error::Error;
 use summary::Summary;
 
 
+/// The results and parameters of a two-sided, unequal-variances t-test.
 pub struct TTest {
     pub p: f64,
     pub t: f64,
@@ -14,6 +15,7 @@ pub fn t_test_2_sided(t: f64, df: f64) -> Result<TTest, Error> {
     Ok(TTest { df, p, t })
 }
 
+/// Conduct a two-sided t-test that does not assume equal population variances.
 pub fn welch_t_test(s1: &Summary, s2: &Summary) -> Result<TTest, Error> {
     let (t, df) = welch_t_statistic(s1, s2);
 
@@ -37,6 +39,9 @@ fn welch_t_statistic(s1: &Summary, s2: &Summary) -> (f64, f64) {
     (t, df)
 }
 
+/// Degrees of freedom, approximated using the Welch-Satterthwaite equation [1].
+///
+/// [1]: http://www.itl.nist.gov/div898/handbook/mpc/section5/mpc571.htm
 fn welch_satterthwaite_df(var1: f64, n1: f64, var2: f64, n2: f64) -> f64 {
     let df1 = n1 - 1.0;
     let df2 = n2 - 1.0;
@@ -48,6 +53,12 @@ fn welch_satterthwaite_df(var1: f64, n1: f64, var2: f64, n2: f64) -> f64 {
     appx
 }
 
+/// The definite integral of the density function of Student's t-distribution
+/// over an interval [-t, t]. Also called the A(t|ν) function.
+///
+/// See equation 6.4.9 in [1].
+///
+/// [1]: "Numerical Recipes in C", 2nd Ed., p. 228
 fn t_atv(t: f64, df: f64) -> Result<f64, Error> {
     use num;
 
